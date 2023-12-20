e5aae28c3f46de9304f58a999d4dd552f4a0ada7
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2023-12-20 12:32:34 +0100

    Implementation of the dimension table dim_products

---
+++ fact_and_dim/dim_products.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from credentials import db_config_silver, db_config_gold
+
+# Establish a connection to the silver PostgreSQL database
+silver_engine = create_engine(
+    f"postgresql+psycopg2://{db_config_silver['user']}:{db_config_silver['password']}@"
+    f"{db_config_silver['host']}:{db_config_silver['port']}/{db_config_silver['database']}"
+)
+
+# Read data from the products table of the silver zone
+dim_products = pd.read_sql_table('trf_products', silver_engine, schema='csv')
+
+# Show the first rows to verify the changes
+pd.set_option('display.max_columns', None)  # Displays all columns
+pd.set_option('display.expand_frame_repr', False)  # Avoids line break in the console output
+print(dim_products.head())
+
+# Establish a connection to the gold PostgreSQL database
+gold_engine = create_engine(
+    f"postgresql+psycopg2://{db_config_gold['user']}:{db_config_gold['password']}@"
+    f"{db_config_gold['host']}:{db_config_gold['port']}/{db_config_gold['database']}"
+)
+
+# Load the transformed data into a new table in the gold zone
+dim_products.to_sql('dim_products', gold_engine, if_exists='replace', index=False, schema='csv')
